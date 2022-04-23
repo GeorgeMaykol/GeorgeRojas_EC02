a8271143758ff7aa11dd75d7c2690e6f3528c063
Maykol Rojas <maykolroj@gmail.com>
2022-04-22 21:31:15 -0500

    inicio de examen

---
+++ src/main/java/com/idat/idat_GeorgeRojas_ec02/IdatGeorgeRojasEc02Application.java
@@ -0,0 +1,13 @@
+package com.idat.idat_GeorgeRojas_ec02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdatGeorgeRojasEc02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdatGeorgeRojasEc02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/idat_GeorgeRojas_ec02/IdatGeorgeRojasEc02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.idat_GeorgeRojas_ec02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdatGeorgeRojasEc02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
